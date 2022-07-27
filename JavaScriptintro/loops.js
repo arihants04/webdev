2416e28c05e8cf49f88b47ebc293d20810a341c5
Ari S <ari@Aris-MacBook-Pro.local>
2022-07-27 16:42:00 -0500

    loops2

--- JavaScriptintro/loops.js
+++ JavaScriptintro/loops.js
@@ -1,3 +1,15 @@
 for (let i = 1; i <= 500; i++) {
     console.log(i)
 }
+
+for (let i = 0; i <= 20; i += 2) {
+    console.log(i)
+}
+
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(i)
+}
+
+for (i = 2; i <= 2048; i = i ** 2) {
+    console.log(i);
+}
