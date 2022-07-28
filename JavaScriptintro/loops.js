4ebe999f76de265cb77c2d8ad4456fcc56b3073f
Ari S <ari@Aris-MacBook-Pro.local>
2022-07-28 01:35:11 -0500

    nested loops

--- JavaScriptintro/loops.js
+++ JavaScriptintro/loops.js
@@ -13,3 +13,24 @@ for (let i = 100; i >= 0; i -= 10) {
 for (i = 2; i <= 2048; i = i ** 2) {
     console.log(i);
 }
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`i is ${i}`)
+    for (let j = 1; j <= 3; j++) {
+        console.log(`          j is ${j}`)
+    }
+}
+
+const seatingChart = [
+    ['Kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+
+for (let i = 0; i < seatingChart.length; i++) {
+    let row = seatingChart[i]
+    console.log(`Row #${i + 1}`)
+    for (let j = 0; j < row.length; j++) {
+        console.log(row[j])
+    }
+}
