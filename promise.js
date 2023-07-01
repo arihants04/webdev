29a9eb877e667864fb6896720263908cc8e355f1
Ari S <ari@aris-mbp.lan>
2023-07-01 03:45:42 -0500

    promises

--- promise.js
+++ promise.js
@@ -84,23 +84,40 @@
 //         console.log('Error! A request failed')
 //     })
 
-const fakeRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        const rand = Math.random()
-        setTimeout(() => {
-            if (rand <= 0.7) {
-                resolve("DATA");
-            }
-            reject("REQUEST ERROR!");
-        }, 1000)
+// const fakeRequest = (url) => {
+//     return new Promise((resolve, reject) => {
+//         const rand = Math.random()
+//         setTimeout(() => {
+//             if (rand <= 0.7) {
+//                 resolve("DATA");
+//             }
+//             reject("REQUEST ERROR!");
+//         }, 1000)
 
+//     })
+// }
+
+// fakeRequest('/dogs/1')
+//     .then((data) => {
+//         console.log("DONE", data)
+//     })
+//     .catch((err) => {
+//         console.log("ERROR ", err)
+//     })
+
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
     })
 }
 
-fakeRequest('/dogs/1')
-    .then((data) => {
-        console.log("DONE", data)
-    })
-    .catch((err) => {
-        console.log("ERROR ", err)
-    })
+delayedColorChange('red', 1000)
+    .then(() => delayedColorChange('orange', 1000))
+    .then(() => delayedColorChange('yellow', 1000))
+    .then(() => delayedColorChange('green', 1000))
+    .then(() => delayedColorChange('blue', 1000))
+    .then(() => delayedColorChange('indigo', 1000))
+    .then(() => delayedColorChange('violet', 1000))
