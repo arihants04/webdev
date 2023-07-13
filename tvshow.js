a5ac31a28b533f93f6ab09d11b9bc6420469c4d7
Ari S <ari@aris-mbp.lan>
2023-07-13 03:37:43 -0500

    tvshowaxios

--- tvshow.js
+++ tvshow.js
@@ -1,7 +1,21 @@
 let form = document.querySelector("#form")
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", async (e) => {
     e.preventDefault();
-    console.log("submitted");
-    let term = form.elements.submit.value;
-    console.dir(form)
+    let allimgs = document.body.querySelectorAll("img");
+    allimgs.forEach(e => e.remove());
+    let term = form.elements.query.value;
+    console.log(term);
+    let config = { params: { q: term } }
+    let response = await axios.get(`http://api.tvmaze.com/search/shows`, config)
+    for (let x = 0; x < 10; x++) {
+        if (response.data[x].show.image) {
+            let img = document.createElement("img")
+            let imgdata = response.data[x].show.image.medium;
+            img.src = imgdata;
+            document.body.append(img)
+        }
+
+    }
+    form.elements.query.value = '';
+    count++;
 })
