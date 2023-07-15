8f31aba4df01d03f907835ac5bfef5fc446cc130
Ari S <ari@aris-mbp.lan>
2023-07-15 03:21:19 -0500

    factory

---
+++ factory.js
@@ -0,0 +1,22 @@
+hex = (r, g, b) => {
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+rgb = (r, g, b) => {
+    return `rgb(${r}, ${g},${b})`;
+}
+
+function makeColor(r, g, b) {
+    const color = {};
+    color.r = r;
+    color.g = g;
+    color.b = b;
+    color.rgb = function () {
+        let { r, g, b } = this;
+        return `rgb(${r}, ${g},${b})`;
+    }
+    color.hex = function () {
+        let { r, g, b } = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+    return color;
+}
