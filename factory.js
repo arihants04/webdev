5e29ef7c745d12bbc71f61ab3bce61e637dc6ef4
Ari S <ari@aris-mbp.lan>
2023-07-16 04:45:46 -0500

    new

--- factory.js
+++ factory.js
@@ -25,7 +25,22 @@ function Color(r, g, b) {
     this.r = r;
     this.g = g;
     this.b = b;
-    console.log(this)
 }
 
-new Color(255, 40, 100);
+Color.prototype.rgb = function () {
+    let { r, g, b } = this;
+    return `rgb(${r},${g},${b})`;
+}
+
+Color.prototype.hex = function () {
+    let { r, g, b } = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+Color.prototype.rgba = function (a = 1.0) {
+    let { r, g, b } = this;
+    return `rgba(${r},${g},${b},${a})`;
+}
+
+let newColor = new Color(255, 40, 100);
+document.body.style.backgroundColor = newColor.rgba(.8)
