8bf76fad442e004712d867c9953a9be9d7bb792c
ari s <arihants04@gmail.com>
2023-07-16 18:25:46 -0500

    classes

---
+++ class.js
@@ -0,0 +1,19 @@
+class Color {
+    constructor(r, g, b, name) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+    }
+
+    rgb() {
+        let { r, g, b } = this;
+        return `rgb(${r}, ${g},${b})`;
+    }
+    hex() {
+        let { r, g, b } = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+}
+
+const c1 = new Color(255, 67, 89, tomato)
