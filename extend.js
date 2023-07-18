e92ea53c219f62db0c4360c81376bf68e482f243
Ari S <ari@Aris-MacBook-Pro.local>
2023-07-18 15:06:55 -0500

    extend

--- extend.js
+++ extend.js
@@ -1,6 +1,24 @@
-class Cat {
+class Pet {
     constructor(name, age) {
         this.name = name;
         this.age = age;
+    }
+    eat() {
+        `${this.name} is eating`;
+    }
+}
+
+class Cat extends Pet {
+    constructor(name, age, lives = 9) {
+        super(name, age);
+        this.lives = lives;
+    }
+    meow() {
+        return `${this.name} meows`
+    }
+}
+class Dog extends Pet {
+    bark() {
+        return `${this.name} barks`
     }
 }
