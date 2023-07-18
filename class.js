aa17da3e0cab28196934813c73559816740efb90
Ari S <ari@aris-mbp.lan>
2023-07-18 03:37:42 -0500

    classes

--- class.js
+++ class.js
@@ -4,6 +4,7 @@ class Color {
         this.g = g;
         this.b = b;
         this.name = name;
+        this.calcHSL();
     }
 
     innerRGB() {
@@ -20,7 +21,64 @@ class Color {
         let { r, g, b } = this;
         return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
     }
+    hsl() {
+        let { h, s, l } = this;
+        return `hsl(${h},${s}%,${l}%)`
+    }
+    opposite() {
+        let { h, s, l } = this;
+        h = (h + 180) % 360
+        return `hsl(${h},${s}%,${l}%)`
+    }
+    fullSaturation() {
+        let { h, l } = this;
+        return `hsl(${h},${100}%,${l}%)`
+    }
+
+    calcHSL() {
+        let { r, g, b } = this;
+        // Make r, g, and b fractions of 1
+        r /= 255;
+        g /= 255;
+        b /= 255;
+
+        // Find greatest and smallest channel values
+        let cmin = Math.min(r, g, b),
+            cmax = Math.max(r, g, b),
+            delta = cmax - cmin,
+            h = 0,
+            s = 0,
+            l = 0;
+        if (delta == 0) h = 0;
+        else if (cmax == r)
+            // Red is max
+            h = ((g - b) / delta) % 6;
+        else if (cmax == g)
+            // Green is max
+            h = (b - r) / delta + 2;
+        else
+            // Blue is max
+            h = (r - g) / delta + 4;
+
+        h = Math.round(h * 60);
+
+        // Make negative hues positive behind 360°
+        if (h < 0) h += 360;
+        // Calculate lightness
+        l = (cmax + cmin) / 2;
+
+        // Calculate saturation
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+        // Multiply l and s by 100
+        s = +(s * 100).toFixed(1);
+        l = +(l * 100).toFixed(1);
+        this.h = h;
+        this.s = s;
+        this.l = l;
+    }
 }
 
 const c1 = new Color(255, 67, 89, 'tomato')
 document.body.style.backgroundColor = c1.rgba(.5)
+document.body.style.backgroundColor = c1.hsl()
