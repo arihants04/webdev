12622fe07b5e24d46f76f73b980b46fa87b9e8b7
Ari S <ari@aris-mbp.lan>
2023-08-06 04:02:08 -0500

    Express

--- firstExpress/index.js
+++ firstExpress/index.js
@@ -2,13 +2,11 @@ const express = require("express");
 const app = express();
 let port = 3000;
 app.listen(port, () => {
-    console.log(`Listening on port ${port}!`)
+    console.log(`Listening on port ${port}!`);
 })
 
-app.use(() => {
-    console.log("We got a new request!")
+app.use((req, res) => {
+    console.log("We got a new request!");
+    res.send("<h1>Response<h1>");
+    // res.send({ color: 'red' });
 })
-
-// app.get('/', (req, res) => {
-//     res.send("Hello world")
-// })
