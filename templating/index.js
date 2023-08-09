7a230e60752fe48d466d01cbc821588bd98bea85
Ari S <ari@aris-mbp.lan>
2023-08-09 05:14:22 -0500

    templating

--- templating/index.js
+++ templating/index.js
@@ -10,6 +10,16 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
     res.render("home.ejs")
 })
+app.get('/rand', (req, res) => {
+    let num = Math.floor(Math.random() * 10) + 1;
+    res.render("rand.ejs", { num });
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    let subreddit = req.params.subreddit
+    //let {subreddit} = req.params
+    res.render("subreddit.ejs", { subreddit });
+})
 
 app.listen(3000, () => {
     console.log('listening on port 3000!')
