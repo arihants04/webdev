3c54e484242c3f7d546d6f4b7eb9247618d31ca3
Ari S <ari@Aris-MacBook-Pro.local>
2023-09-12 18:03:45 -0500

    message

--- spam.py
+++ spam.py
@@ -4,13 +4,13 @@ import pyautogui
 
 def spam():
 
-    text = open("/Users/ari/Desktop/html/message.txt")
+    text = open("/Users/ari/Desktop/webdev/message.txt")
     for each_line in text:
         pyautogui.typewrite(each_line)
         pyautogui.press('return')
 
 
-time.sleep(5)
+time.sleep(10)
 r = 0
 for r in range(100):
     spam()
