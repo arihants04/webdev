3e174b1b0cd930f9ad14e1585ccf91be9ec87867
arihants04 <106578346+arihants04@users.noreply.github.com>
2023-11-01 02:22:56 -0500

    Update finance/index.tsx

--- finance/index.tsx
+++ finance/index.tsx
@@ -2,18 +2,20 @@ import Chart from 'chart.js/auto';
 import { useRef, useEffect } from 'react';
 
 export default function finance() {
-  const canvas = useRef(null);
+  const estimatedCanvas = useRef(null);
+  const actualCanvas = useRef(null);
 
   useEffect(() => {
-    const ctx = canvas.current;
+    const estimatedCtx = estimatedCanvas.current;
+    const actualCtx = actualCanvas.current;
 
     let chartStatus = Chart.getChart('myChart');
     if (chartStatus != undefined) {
       chartStatus.destroy();
     }
 
-    if (ctx !== null) {
-      const chart = new Chart(ctx, {
+    if (estimatedCtx !== null) {
+      const chart = new Chart(estimatedCtx, {
         type: 'pie',
         data: {
           labels: ['Research Funding', 'Dev funding', 'Food', 'Merchandise', 'Other'],
@@ -46,10 +48,61 @@ export default function finance() {
           plugins: {
             legend: {
               position: 'top',
+              labels: {
+                color: 'white', // Set the label text color to white
+              },
             },
             title: {
               display: true,
               text: 'Estimated Budget',
+              color: 'white',
+            },
+          },
+        },
+      });
+    }
+    if (actualCtx !== null) {
+      const chart = new Chart(actualCtx, {
+        type: 'pie',
+        data: {
+          labels: ['Research Funding', 'Dev funding', 'Food', 'Merchandise', 'Other'],
+          datasets: [
+            {
+              label: '% of Budget',
+              data: [12, 19, 3, 2, 3],
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 05)',
+                'rgba(255, 159, 64, 0.5)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'top',
+              labels: {
+                color: 'white', // Set the label text color to white
+              },
+            },
+            title: {
+              display: true,
+              text: 'Actual Budget',
+              color: 'white',
             },
           },
         },
@@ -58,8 +111,12 @@ export default function finance() {
   }, []);
 
   return (
-    <div className="container h-96">
-      <canvas ref={canvas}></canvas>
-    </div>
+    <>
+      <div className="pb-24"></div>
+      <div className="container h-96 flex cols cols-2 pl-24">
+        <canvas ref={estimatedCanvas}></canvas>
+        <canvas ref={actualCanvas}></canvas>
+      </div>
+    </>
   );
 }
