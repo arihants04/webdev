0362955583763f33e4f17e5ac1879d1514d0416a
Ari S <ari@Aris-MacBook-Pro.local>
2023-11-26 15:54:30 -0600

    comments get post

--- Rest/index.js
+++ Rest/index.js
@@ -1,43 +1,43 @@
 const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.set('views', path.join(__dirname,'views'));
+app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 const comments = [
-    {username: "Todd",
-    comment: "LOL"    
-    },
-    {username: "Walt",
-    comment: "So funny"    
-    },
-    {username: "Nolan",
-    comment: "Haha"    
-    },
-    {username: "Mark",
-    comment: "Hilarous!!"    
-    },
+  { username: "Todd", comment: "LOL" },
+  { username: "Walt", comment: "So funny" },
+  { username: "Nolan", comment: "Haha" },
+  { username: "Mark", comment: "Hilarous!!" },
+];
 
-]
+app.get("/comments", (req, res) => {
+  res.render("comments/index", { comments });
+});
 
-app.get('/comments',(req,res)=>{
-    res.render('comments/index');
-})
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new");
+});
 
-app.get('/tacos',(req,res)=>{
-    res.send("GET response for /tacos")
-})
+app.post("/comments", (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ username, comment });
+  res.redirect("/comments");
+});
 
+app.get("/tacos", (req, res) => {
+  res.send("GET response for /tacos");
+});
 
-app.post('/tacos',(req,res)=>{
-const {tacotype,qty} = req.body;
-res.send(`Here is ${qty} ${tacotype} tacos`);
-})
+app.post("/tacos", (req, res) => {
+  const { tacotype, qty } = req.body;
+  res.send(`Here is ${qty} ${tacotype} tacos`);
+});
 
-app.listen(3000,()=>{
-    console.log("On port 3000!");
-})
+app.listen(3000, () => {
+  console.log("On port 3000!");
+});
